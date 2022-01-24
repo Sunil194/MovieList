d60ae8b309398935e4b87f5b44ca92d63fbfffcd
Sunil194 <85332465+Sunil194@users.noreply.github.com>
2022-01-24 12:02:31 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -5,10 +5,10 @@ import MovieList from './components/MovieList';
 
 function App() {
   const [ movies, setMovies]= useState([]);
-  function fetchDataHandler() {
-    fetch('https://swapi.dev/api/films').then(response => {
-      return response.json();
-    }).then(data => {
+  async function fetchDataHandler() {
+    const response= await fetch('https://swapi.dev/api/films');
+    const data = await response.json();
+    
       const transformMovies = data.results.map(movieData => {
       return{
         id: movieData.episode_id,
@@ -17,7 +17,6 @@ function App() {
       }
     });
       setMovies(transformMovies);
-    })
   }
   return (
     <div className="App">
